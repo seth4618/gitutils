a6f7ba3c11ad088ee282b934fbe240a4cfdb337e
Seth Copen Goldstein <seth@cmu.edu>
2023-07-25 18:54:25 -0400

    remove fragment which was for testing

--- org2/openlink.py
+++
@@ -1,9 +0,0 @@
-#!/home/seth/.virtualenvs/gitutils/bin/python
-
-import webbrowser
-
-url = 'http://docs.python.org/'
-webbrowser.open("about:blank", new=1)
-webbrowser.open("https://github.com/monatized/sharedweb/pull/619", new=2)
-
-
